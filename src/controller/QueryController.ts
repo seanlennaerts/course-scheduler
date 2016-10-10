bf17ef03bbd23f07a5781570d1baf3b80069ae49
Ana Barajas <anabarajas@live.com.mx>
2016-10-09 21:27:33 -0700

    got isValid in QueryController working almost completely! except for ORDER

--- src/controller/QueryController.ts
+++ src/controller/QueryController.ts
@@ -8,7 +8,6 @@ import Course from "../model/Course";
 //import Section from "../model/Section";
 import RouteHandler from "../rest/RouteHandler"
 
-
 export interface QueryRequest {
     GET: string[]; //has to be string array -S
     WHERE: {};
@@ -26,7 +25,7 @@ export default class QueryController {
     private tempResults: Course[][] = [];
     private dataset: Course[] = [];
     private queryKeys: string[] = [];
-    //private datasetID: string;
+    private wrongDatasetIDs: string[];
 
     constructor(datasets: Datasets) {
         this.datasets = datasets;
@@ -35,72 +34,100 @@ export default class QueryController {
 
     }
 
-    private WHEREhelperArray(array:{}[]):boolean {
-        for(var i = 0; i < array.length; i++){
-            this.WHEREhelperObject(array[i]);
-        }
-        return true;
+    public returnWrongIDs(): string[]{
+        return this.wrongDatasetIDs
     }
 
-    private objectOrArrayValid (checkObj: any) {
+    private WHEREhelperArray(array:{}[]): number {
+        var resultNumbers :number[] = [];
+        for(var i of array){
+            resultNumbers.push(this.WHEREhelperObject(i));
+        }
+        if (resultNumbers.indexOf(400,0) === -1){
+            Log.info("QueryController :: WHEREhelperArray(..) - Array of objects all return 200!");
+            return 200;
+        }else{
+            Log.info("QueryController :: WHEREhelperArray(..) - At least one object in the array returns 400");
+            return 400;
+        }
+    }
+
+    private objectOrArrayValid (checkObj: any): number {
         if (typeof(checkObj) === {}) {
-            this.WHEREhelperObject(checkObj);
+            Log.info("QueryController :: objectOrArrayValid(..) - it's an object, now going to the helper");
+            return this.WHEREhelperObject(checkObj);
         } else {
-            this.WHEREhelperArray(checkObj);
+            Log.info("QueryController :: objectOrArrayValid(..) - it's an array, now going to helper");
+            return this.WHEREhelperArray(checkObj);
         }
     }
 
-    private WHEREhelperObject(whereObject: {}):boolean {
+    private WHEREhelperObject(whereObject: {}): number {
+        let that = this;
         // section 1
         if (Object.keys(whereObject).length > 1){
-            Log.info("QueryController :: WHEREhelperObject(..) - about to return false: sect 1");
-            return false;
+            Log.info("QueryController :: WHEREhelperObject(..) - Object has more than one key");
+            return 400;
         }
         // section 2
         // check if the object key is one of the permitted course keys
-        if (Object.keys(whereObject)[0] === "dept" || Object.keys(whereObject)[0] === "id" ||
-                Object.keys(whereObject)[0] === "avg" || Object.keys(whereObject)[0] === "instructor" ||
-                Object.keys(whereObject)[0] === "title" || Object.keys(whereObject)[0] === "pass" ||
-                Object.keys(whereObject)[0] === "fail"){
-            Log.info("QueryController :: WHEREhelperObject(..) - about to return true: sect 2 ");
-            return true;
+        if (Object.keys(whereObject)[0].split("_")[1] === "dept" || Object.keys(whereObject)[0].split("_")[1]  === "id" ||
+                Object.keys(whereObject)[0].split("_")[1]  === "avg" || Object.keys(whereObject)[0].split("_")[1]  === "instructor" ||
+                Object.keys(whereObject)[0].split("_")[1]  === "title" || Object.keys(whereObject)[0].split("_")[1]  === "pass" ||
+                Object.keys(whereObject)[0].split("_")[1]  === "fail"){
+            Log.info("QueryController :: WHEREhelperObject(..) - reached base case (no more nested objects/arrays), object key is " + Object.keys(whereObject)[0]);
+            return 200;
         }
         // section 3
         // check if the keys in first level are the LOGIC operators allowed
-        if (!(Object.keys(whereObject)[0] === "LT" || Object.keys(whereObject)[0] === "GT" ||
-            Object.keys(whereObject)[0] === "EQ" || Object.keys(whereObject)[0] === "AND" ||
-            Object.keys(whereObject)[0] === "OR" || Object.keys(whereObject)[0] === "IS" ||
-            Object.keys(whereObject)[0] === "NOT")){
-            Log.info("QueryController :: WHEREhelperObject(..) - about to return false: sect 3 ");
-            return false;
-        } else {
-            var secondLevel: {}|{}[] = Object.values(whereObject)[0];
-            this.objectOrArrayValid(secondLevel);
+        // TODO: when object operators are not the given ones
+        if (Object.keys(whereObject)[0] === "LT" || Object.keys(whereObject)[0] === "GT" ||
+            Object.keys(whereObject)[0] === "EQ" ||  Object.keys(whereObject)[0] === "IS" ||
+            Object.keys(whereObject)[0] === "NOT") {
+            var objectKey: string = Object.keys(whereObject)[0];
+            var secondLevel: {} = whereObject[objectKey];
+            Log.info("QueryController :: WHEREhelperObject(..) - about to go in a level deeper recursively, with this object: " + JSON.stringify(secondLevel));
+            return that.WHEREhelperObject(secondLevel);
         }
-        Log.info("QueryController :: WHEREhelperObject(..) - about to return true in the end ");
-        return true;
+        else if (Object.keys(whereObject)[0] === "AND" || Object.keys(whereObject)[0] === "OR"){
+            var objectKey: string = Object.keys(whereObject)[0];
+            var secondLevelArray: {}[] = whereObject[objectKey];
+            Log.info("QueryController :: WHEREhelperObject(..) - about to go in a level deeper recursively, with this array: " + JSON.stringify(secondLevel));
+            return that.WHEREhelperArray(secondLevelArray);
+        } else{
+            Log.info("QueryController :: WHEREhelperObject(..) - about to return errors, these object keys are not proper operators: " + Object.keys(whereObject)[0]);
+            return 400;
+        }
     }
 
-    public isValid(query: QueryRequest): boolean {
-        if (typeof query !== 'undefined' && query !== null && Object.keys(query).length > 3) {
-            if (query.GET && query.WHERE && query.AS){
+    public isValid(query: QueryRequest): number {
+        if (typeof query !== 'undefined' && query !== null && Object.keys(query).length >= 3) {
+            if (query.GET && query.WHERE && query.AS) {
                 // GET part of query
                 var GETelements: string[] = query.GET;
+                Log.info("QueryController :: isValid(..) - GETelements are: " + JSON.stringify(GETelements));
                 if (GETelements.length > 0) {
                     for (var i = 0; i < GETelements.length; i++) {
                         var GETelement: string[] = GETelements[i].split("_");
-                        var id = GETelement[0];
+                        var id: string = GETelement[0];
+                        Log.info("QueryController :: isValid(..) - id is: " + id);
                         if (!(id in this.datasets)) {
-                            Log.info("QueryController :: isValid(..) - about to return false in GET in for loop ");
-                            return false;
-                        } else {
+                            this.wrongDatasetIDs.push(id);
+                            Log.info("QueryController :: isValid(..) - about to return 424 error: " + id + "hasn't been put");
+                            Log.info("QueryController :: isValid(..) - the wrongDatasetIDS are " + JSON.stringify(this.wrongDatasetIDs));
+                            return (424);
+                        }
+                        Log.info("QueryController :: isValid(..) - id has been put");
+                        else {
                             var datasetField = GETelement[1];
+                            Log.info("QueryController :: isValid(..) - datasetField is: " + datasetField);
                             if (!(datasetField === "dept" || datasetField === "id" || datasetField === "avg" ||
                                 datasetField === "instructor" || datasetField === "title" ||
                                 datasetField === "pass" || datasetField === "fail" || datasetField === "audit")) {
-                                Log.info("QueryController :: isValid(..) - about to return false in GET in else ");
-                                return false;
+                                Log.info("QueryController :: isValid(..) - wrong field in query submitted ");
+                                return 400;
                             } else {
+                                Log.info("QueryController :: isValid(..) - pushing datasetField: " + datasetField);
                                 this.queryKeys.push(datasetField);
                             }
                         }
@@ -108,28 +135,30 @@ export default class QueryController {
                 }
                 // AS part of query
                 var as = query.AS;
-                if (!(as == "TABLE")){
-                    Log.info("QueryController :: isValid(..) - about to return false in AS");
-                    return false;
+                if (!(as == "TABLE")) {
+                    Log.info("QueryController :: isValid(..) - AS is other than 'TABLE'");
+                    return 400;
                 }
+                Log.info("QueryController :: isValid(..) - AS is'TABLE' :) ");
                 // ORDER part of query -> OPTIONAL
-                if (query.ORDER){
-                    for (var key in this.queryKeys){
-                        if (query.ORDER == key){
-                            this.WHEREhelperObject(query.WHERE);
-                        } else {
-                            Log.info("QueryController :: isValid(..) - about to return false in ORDER");
-                            return false;
-                        }
+                if (query.ORDER) {
+                    if (this.queryKeys.indexOf(query.ORDER, 0) !== -1) {
+                        Log.info("QueryController :: isValid(..) - ORDER key is not included in GET keys");
+                        return 400;
+                    } else {
+                        Log.info("QueryController :: isValid(..) - there is ORDER key included in GET keys, going to WHERE helper ");
+                        return this.WHEREhelperObject(query.WHERE);
                     }
+                } else {
+                    Log.info("QueryController :: isValid(..) - no ORDER key, query is now going to  WHEREhelperObject");
+                    return this.WHEREhelperObject(query.WHERE);
                 }
-                return this.WHEREhelperObject(query.WHERE);
             }
-            Log.info("QueryController :: isValid(..) - about to return false close to the end");
-            return false;
+            Log.info("QueryController :: isValid(..) - query doesn't include GET, WHERE, AS");
+            return 400;
         }
-        Log.info("QueryController :: isValid(..) - about to return false at the very end ");
-        return false;
+        Log.info("QueryController :: isValid(..) - query is either undefined, null, or keys are less than 3 ");
+        return 400;
     }
 
     public getDataset(id: string): Course[]{
