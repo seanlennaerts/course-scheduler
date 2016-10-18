fc2de9caa6b4b4ee3cf97658dc9bddc632eaab4d
Sean Lennaerts <seanlenny@gmail.com>
2016-10-17 20:03:06 -0700

    All given unit tests passing

--- test/QueryControllerSpec.ts
+++ test/QueryControllerSpec.ts
@@ -2,28 +2,13 @@
  * Created by rtholmes on 2016-10-31.
  */
 
-import fs = require('fs');
 import {Datasets} from "../src/controller/DatasetController";
 import QueryController from "../src/controller/QueryController";
 import {QueryRequest} from "../src/controller/QueryController";
 import Log from "../src/Util";
-
 import {expect} from 'chai';
-import InsightFacade from "../src/controller/InsightFacade";
-describe("QueryController", function () {
 
-    var zipFileContents: string = null;
-    var facade: InsightFacade = new InsightFacade();
-    before(function () {
-        zipFileContents = new Buffer(fs.readFileSync('courses.zip')).toString('base64');
-        try {
-            fs.unlinkSync("./data/courses.json");
-        } catch (err) {
-            Log.warn('InsightController::before() - courses.json not removed (probably not present)');
-        }
-        facade = new InsightFacade();
-        facade.addDataset("courses", zipFileContents);
-    });
+describe("QueryController", function () {
 
     beforeEach(function () {
     });
@@ -31,8 +16,31 @@ describe("QueryController", function () {
     afterEach(function () {
     });
 
-    it("Should be able to validate a valid query", function () {
-        // NOTE: this is not actually a valid qf`uery for D1
+    it("Valid query and dataset exists, should return 200", function () {
+        // NOTE: this is not actually a valid query for D1
+        let query: QueryRequest = {
+            "GET": ["courses_dept", "courses_avg"],
+            "WHERE": {"GT": {"courses_avg": 90}},
+            "ORDER": "courses_avg",
+            "AS": "TABLE"
+        };
+        let dataset: Datasets = {courses: []}; //just checking query validity so need dataset id "courses" to exist -S
+        let controller = new QueryController(dataset);
+        let isValid = controller.isValid(query);
+
+        expect(isValid).to.equal(200);
+    });
+
+    it("Invalid query and dataset exists, should return 400", function () {
+        let query: any = null;
+        let dataset: Datasets = {courses: []};
+        let controller = new QueryController(dataset);
+        let isValid = controller.isValid(query);
+
+        expect(isValid).to.equal(400);
+    });
+
+    it("Valid query and dataset doesn't exist, should return 424", function () {
         let query: QueryRequest = {
             "GET": ["courses_dept", "courses_avg"],
             "WHERE": {"GT": {"courses_avg": 90}},
@@ -42,32 +50,24 @@ describe("QueryController", function () {
         let dataset: Datasets = {};
         let controller = new QueryController(dataset);
         let isValid = controller.isValid(query);
+        let missingIDs = controller.returnWrongIDs();
 
-        expect(isValid).to.equal(200);
-    });
-
-    it("Should be able to invalidate an invalid query", function () {
-        let query: any = null;
-        let dataset: Datasets = {};
-        let controller = new QueryController(dataset);
-        let isValid = controller.isValid(query);
-
-        expect(isValid).to.equal(false);
+        expect(isValid).to.equal(424);
+        expect(missingIDs[0]).to.equal("courses");
     });
 
     it("Should be able to query, although the answer will be empty", function () {
-        // NOTE: this is not actually a valid query for D1, nor is the result correct.
         let query: QueryRequest = {
             "GET": ["courses_dept", "courses_avg"],
             "WHERE": {"GT": {"courses_avg": 200}},
             "ORDER": "courses_avg",
             "AS": "TABLE"
         };
-        let dataset: Datasets = {};
+        let dataset: Datasets = {courses: []};
         let controller = new QueryController(dataset);
         let ret = controller.query(query);
         Log.test('In: ' + JSON.stringify(query) + ', out: ' + JSON.stringify(ret));
         expect(ret).not.to.be.equal(null);
-        // should check that the value is meaningful
+        expect(ret.result.length).to.equal(0);
     });
 });
