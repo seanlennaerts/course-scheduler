b974583136e37d8a48e5a11181a38876aa72e623
Ana Barajas <anabarajas@live.com.mx>
2016-10-17 22:54:27 -0700

    More tests :)

--- test/QueryControllerSpec.ts
+++ test/QueryControllerSpec.ts
@@ -40,11 +40,11 @@ describe("QueryController", function () {
         expect(isValid).to.equal(400);
     });
 
-    it("Should be able to invalidate an invalid query - wrong mcomparator", function () {
+    it("Should be able to invalidate an invalid query - empty GET array", function () {
         let query: QueryRequest = {
-            "GET": ["courses_dept", "courses_avg"],
-            "WHERE" : {"GET" : {"courses_avg" : 90}},
-            "ORDER" : "courses_pass",
+            "GET": [],
+            "WHERE" : {"GT" : {"courses_avg" : 90}},
+            "ORDER" : "courses_avg",
             "AS" : "TABLE"
         };
         let dataset: Datasets = {};
@@ -54,11 +54,11 @@ describe("QueryController", function () {
         expect(isValid).to.equal(400);
     });
 
-    it("Should be able to invalidate an invalid query - wrong field", function () {
+    it("Should be able to invalidate an invalid query - empty WHERE object", function () {
         let query: QueryRequest = {
             "GET": ["courses_dept", "courses_avg"],
-            "WHERE" : {"GT" : {"courses_avo" : 90}},
-            "ORDER" : "courses_pass",
+            "WHERE" : {},
+            "ORDER" : "courses_avg",
             "AS" : "TABLE"
         };
         let dataset: Datasets = {};
@@ -68,39 +68,11 @@ describe("QueryController", function () {
         expect(isValid).to.equal(400);
     });
 
-    it("Should be able to validate a query without ORDER", function () {
+    it("Should be able to invalidate an invalid query - empty ORDER string", function () {
         let query: QueryRequest = {
             "GET": ["courses_dept", "courses_avg"],
             "WHERE" : {"GT" : {"courses_avg" : 90}},
-            "AS" : "TABLE"
-        };
-        let dataset: Datasets = {};
-        let controller = new QueryController(dataset);
-        let isValid = controller.isValid(query);
-
-        expect(isValid).to.equal(200);
-    });
-
-
-    it("Should be able to invalidate an invalid query - wrong AS key", function () {
-        let query: QueryRequest = {
-            "GET": ["courses_dept", "courses_avg"],
-            "WHERE" : {"GET" : {"courses_avg" : 90}},
-            "ORDER" : "courses_pass",
-            "AS" : "TABL"
-        };
-        let dataset: Datasets = {};
-        let controller = new QueryController(dataset);
-        let isValid = controller.isValid(query);
-
-        expect(isValid).to.equal(400);
-    });
-
-    it("Should be able to invalidate an invalid query - wrong dataset ID in GET", function () {
-        let query: QueryRequest = {
-            "GET": ["courses_dept", "course_avg"],
-            "WHERE" : {"GET" : {"courses_avg" : 90}},
-            "ORDER" : "courses_pass",
+            "ORDER" : "",
             "AS" : "TABLE"
         };
         let dataset: Datasets = {};
@@ -110,13 +82,12 @@ describe("QueryController", function () {
         expect(isValid).to.equal(400);
     });
 
-
-    it("Should be able to invalidate an invalid query - ORDER key not in GET key ", function () {
+    it("Should be able to invalidate an invalid query - empty AS string", function () {
         let query: QueryRequest = {
             "GET": ["courses_dept", "courses_avg"],
             "WHERE" : {"GT" : {"courses_avg" : 90}},
-            "ORDER" : "courses_pass",
-            "AS" : "TABLE"
+            "ORDER" : "courses_avg",
+            "AS" : ""
         };
         let dataset: Datasets = {};
         let controller = new QueryController(dataset);
@@ -153,7 +124,177 @@ describe("QueryController", function () {
         expect(isValid).to.equal(200);
     });
 
-    it("Shoulb be a valid nested three-level query", function () {
+    it("Should be able to validate query - wrong order of query keys", function () {
+        let query: QueryRequest = {
+            "GET": ["courses_dept", "courses_id", "courses_avg"],
+            "AS": "TABLE",
+            "WHERE": {"OR": [{"AND": [{"GT": {"courses_avg": 70}},{"IS": {"courses_dept": "adhe"}}]},{"EQ": {"courses_avg": 90}}]},
+            "ORDER": "courses_avg"
+        };
+        let dataset: Datasets = {};
+        let controller = new QueryController(dataset);
+        let isValid = controller.isValid(query);
+
+        expect(isValid).to.equal(200);
+    });
+
+    it("Should be able to invalidate an invalid query - wrong mcomparator", function () {
+        let query: QueryRequest = {
+            "GET": ["courses_dept", "courses_avg"],
+            "WHERE" : {"GET" : {"courses_avg" : 90}},
+            "ORDER" : "courses_pass",
+            "AS" : "TABLE"
+        };
+        let dataset: Datasets = {};
+        let controller = new QueryController(dataset);
+        let isValid = controller.isValid(query);
+
+        expect(isValid).to.equal(400);
+    });
+
+    it("Should be able to invalidate an invalid query - wrong datasetID in ORDER", function () {
+        let query: QueryRequest = {
+            "GET": ["courses_dept", "courses_avg"],
+            "WHERE" : {"GT" : {"courses_avg" : 90}},
+            "ORDER" : "course_pass",
+            "AS" : "TABLE"
+        };
+        let dataset: Datasets = {};
+        let controller = new QueryController(dataset);
+        let isValid = controller.isValid(query);
+
+        expect(isValid).to.equal(400);
+    });
+
+    it("Should be able to invalidate an invalid query - typo in ORDER", function () {
+        let query: QueryRequest = {
+            "GET": ["courses_dept", "courses_avg"],
+            "WHERE" : {"GT" : {"courses_avg" : 90}},
+            "ODER" : "course_pass",
+            "AS" : "TABLE"
+        };
+        let dataset: Datasets = {};
+        let controller = new QueryController(dataset);
+        let isValid = controller.isValid(query);
+
+        expect(isValid).to.equal(400);
+    });
+
+    it("Should be able to invalidate an invalid query - wrong field in WHERE", function () {
+        let query: QueryRequest = {
+            "GET": ["courses_dept", "courses_avg"],
+            "WHERE" : {"GT" : {"courses_avo" : 90}},
+            "ORDER" : "courses_pass",
+            "AS" : "TABLE"
+        };
+        let dataset: Datasets = {};
+        let controller = new QueryController(dataset);
+        let isValid = controller.isValid(query);
+
+        expect(isValid).to.equal(400);
+    });
+
+    it("Should be able to validate a query without ORDER", function () {
+        let query: QueryRequest = {
+            "GET": ["courses_dept", "courses_avg"],
+            "WHERE" : {"GT" : {"courses_avg" : 90}},
+            "AS" : "TABLE"
+        };
+        let dataset: Datasets = {};
+        let controller = new QueryController(dataset);
+        let isValid = controller.isValid(query);
+
+        expect(isValid).to.equal(200);
+    });
+
+
+    it("Should be able to invalidate an invalid query - wrong AS key", function () {
+        let query: QueryRequest = {
+            "GET": ["courses_dept", "courses_avg"],
+            "WHERE" : {"GT" : {"courses_avg" : 90}},
+            "ORDER" : "courses_pass",
+            "AS" : "TABL"
+        };
+        let dataset: Datasets = {};
+        let controller = new QueryController(dataset);
+        let isValid = controller.isValid(query);
+
+        expect(isValid).to.equal(400);
+    });
+
+    it("Should be able to invalidate an invalid query - dataset ID in WHERE not yet PUT", function () {
+        let query: QueryRequest = {
+            "GET": ["courses_dept", "course_avg"],
+            "WHERE" : {"GT" : {"courses_avg" : 90}},
+            "ORDER" : "courses_pass",
+            "AS" : "TABLE"
+        };
+        let dataset: Datasets = {};
+        let controller = new QueryController(dataset);
+        let isValid = controller.isValid(query);
+
+        expect(isValid).to.equal(424);
+    });
+
+    it("Should be able to invalidate an invalid query - key in GET not valid", function () {
+        let query: QueryRequest = {
+            "GET": ["courses_depto", "course_avg"],
+            "WHERE" : {"GT" : {"courses_avg" : 90}},
+            "ORDER" : "courses_pass",
+            "AS" : "TABLE"
+        };
+        let dataset: Datasets = {};
+        let controller = new QueryController(dataset);
+        let isValid = controller.isValid(query);
+
+        expect(isValid).to.equal(400);
+    });
+
+
+    it("Should be able to invalidate an invalid query - ORDER key not in GET key ", function () {
+        let query: QueryRequest = {
+            "GET": ["courses_dept", "courses_avg"],
+            "WHERE" : {"GT" : {"courses_avg" : 90}},
+            "ORDER" : "courses_pass",
+            "AS" : "TABLE"
+        };
+        let dataset: Datasets = {};
+        let controller = new QueryController(dataset);
+        let isValid = controller.isValid(query);
+
+        expect(isValid).to.equal(400);
+    })
+
+    it("Should be able to invalidate complex query - no datasetID in ORDER", function () {
+        let query: QueryRequest = {
+            "GET": ["courses_dept", "courses_id", "courses_avg"],
+            "WHERE": {"OR": [{"AND": [{"GT": {"courses_avg": 70}},{"IS": {"courses_dept": "adhe"}}]},{"EQ": {"courses_avg": 90}}]},
+            "ORDER": "avg",
+            "AS": "TABLE"
+        };
+        let dataset: Datasets = {};
+        let controller = new QueryController(dataset);
+        let isValid = controller.isValid(query);
+
+        expect(isValid).to.equal(400);
+    });
+
+    it("Should be able to invalidate complex query - no datasetID in GET", function () {
+        let query: QueryRequest = {
+            "GET": ["dept", "courses_id", "courses_avg"],
+            "WHERE": {"OR": [{"AND": [{"GT": {"courses_avg": 70}},{"IS": {"courses_dept": "adhe"}}]},{"EQ": {"courses_avg": 90}}]},
+            "ORDER": "avg",
+            "AS": "TABLE"
+        };
+        let dataset: Datasets = {};
+        let controller = new QueryController(dataset);
+        let isValid = controller.isValid(query);
+
+        expect(isValid).to.equal(400);
+    });
+
+
+    it("Should be a valid nested three-level query", function () {
         let query: QueryRequest = {
             "GET": [
                 "courses_dept",
@@ -170,8 +311,6 @@ describe("QueryController", function () {
 
         expect(isValid).to.equal(200);
     });
-
-
 
     // it("Should be able to query, although the answer will be empty", function () {
     //     // NOTE: this is not actually a valid query for D1, nor is the result correct.
